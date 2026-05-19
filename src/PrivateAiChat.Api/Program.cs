23eedddd22888b336d6483b07f09f4157b3f358c
Amin Chahardoli <aristhewonder@gmail.com>
2026-05-19 09:54:27 +0400

    add nginx reverse proxy

--- src/PrivateAiChat.Api/Program.cs
+++ src/PrivateAiChat.Api/Program.cs
@@ -44,7 +44,6 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
--- src/PrivateAiChat.Web/Program.cs
+++ src/PrivateAiChat.Web/Program.cs
@@ -33,9 +33,6 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
